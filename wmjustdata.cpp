158477035f487d7cf8a110ea7521be1071774a7d
Peter Lohmer <peterlohmer01@gmail.com>
2020-11-27 07:49:47 +0100

    wmjustdata.cpp,h: the class for "old" adjustment data
    Signed-off-by: Peter Lohmer <peterlohmer01@gmail.com>

--- wmjustdata.cpp
+++ wmjustdata.cpp
@@ -1,13 +1,16 @@
 // implemention cWMJustData
 
-#include <qdatastream.h>
-#include <q3textstream.h>
+#include "zeraglobal.h"
+#include "justdata.h"
 #include "wmjustdata.h"
 
 
 cWMJustData::cWMJustData()
 {
-    setDefault();
+    m_pGainCorrection = new cJustData(1, 1.0); // 1.ordnung
+    m_pPhaseCorrection = new cJustData(3, 0.0); // 3.ordnung
+    m_pOffsetCorrection =  new cJustData(0, 0.0); // 0.te ordnung
+    setStatus(0);
 }
 
 
@@ -24,6 +27,7 @@ void cWMJustData::Serialize(QDataStream& qds)  // zum schreiben aller justagedat
     m_pGainCorrection->Serialize(qds); 
     m_pPhaseCorrection->Serialize(qds);
     m_pOffsetCorrection->Serialize(qds);
+    qds << m_nStatus; // die alten justage daten haben nur einen status
 }
  
 
@@ -32,18 +36,32 @@ void cWMJustData::Deserialize(QDataStream& qds) // zum lesen aller justagedaten 
     m_pGainCorrection->Deserialize(qds); 
     m_pPhaseCorrection->Deserialize(qds);
     m_pOffsetCorrection->Deserialize(qds);
+    qds >> m_nStatus; // die alten justage daten haben nur einen status
+}
+
+
+QString cWMJustData::SerializeStatus()
+{
+    return QString("%1").arg(m_nStatus);
+}
+
+
+void cWMJustData::DeserializeStatus(QString s)
+{
+    m_nStatus = s.toInt();
 }
 
 
 int cWMJustData::getStatus()
 {
-    return (m_pGainCorrection->getStatus() & m_pPhaseCorrection->getStatus() & m_pOffsetCorrection->getStatus());
+    if ((m_nStatus & (RangeGainJustified + RangePhaseJustified)) == (RangeGainJustified + RangePhaseJustified))
+        return 1;
+    else
+        return 0;
 }
 
 
-void cWMJustData::setDefault()
+void cWMJustData::setStatus(int stat)
 {
-    m_pPhaseCorrection = new cJustData(PhaseCorrOrder, 0.0);
-    m_pGainCorrection = new cJustData(GainCorrOrder, 1.0);
-    m_pOffsetCorrection =  new cJustData(OffsetCorrOrder, 0.0);
+    m_nStatus = stat;
 }
